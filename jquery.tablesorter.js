f415cfa2c1758a5a85cd1c3f332e3dbcbba8ccfb
Robert Abramski <iam@robertabramski.com>
2014-07-09 15:46:44 -0700

    Check for jQuery.browser.
    In newer versions of jQuery, $.browser is depreciated. Added a check for the browser object.

--- jquery.tablesorter.js
+++ jquery.tablesorter.js
@@ -865,7 +865,7 @@
                 return /^[-+]?\d*$/.test($.trim(s.replace(/[,.']/g, '')));
             };
             this.clearTableBody = function (table) {
-                if ($.browser.msie) {
+                if ($.browser && $.browser.msie) {
                     while (table.tBodies[0].firstChild) {
                         table.tBodies[0].removeChild(table.tBodies[0].firstChild);
                     }
