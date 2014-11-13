f9bebe8d8c5f3c6aaaaf4dd45ef9c20664f18f16
lucasandrade <lucas.andrade@oi.net.br>
2014-11-13 14:35:05 -0200

    Included string extension to remove accents.

--- jquery.tablesorter.js
+++ jquery.tablesorter.js
@@ -890,7 +890,7 @@
         is: function (s) {
             return true;
         }, format: function (s) {
-            return $.trim(s.toLocaleLowerCase());
+            return $.trim(s.toLocaleLowerCase().cleanAccents());
         }, type: "text"
     });
 
@@ -1036,3 +1036,31 @@
         }
     });
 })(jQuery);
+
+// Convert characters with accents to normal
+String.prototype.cleanAccents = function () {
+    var string = this;
+    var mapaAcentosHex = {
+        a: /[\xE0-\xE6]/g,
+        A: /[\xC0-\xC6]/g,
+        e: /[\xE8-\xEB]/g,
+        E: /[\xC8-\xCB]/g,
+        i: /[\xEC-\xEF]/g,
+        I: /[\xCC-\xCF]/g,
+        o: /[\xF2-\xF6]/g,
+        O: /[\xD2-\xD6]/g,
+        u: /[\xF9-\xFC]/g,
+        U: /[\xD9-\xDC]/g,
+        c: /\xE7/g,
+        C: /\xC7/g,
+        n: /\xF1/g,
+        N: /\xD1/g,
+    };
+
+    for (var letra in mapaAcentosHex) {
+        var expressaoRegular = mapaAcentosHex[letra];
+        string = string.replace(expressaoRegular, letra);
+    }
+
+    return string;
+}
